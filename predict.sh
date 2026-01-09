acbf9ebc6bee00d9e031e6732488a6e2803ea2cb
UgoPelissier <upelissier@gmail.com>
2026-01-09 18:46:41 +0100

    update predict config

--- predict.sh
+++ predict.sh
@@ -1,5 +1,7 @@
+export WANDB_MODE=offline
+
 python -m graphphysics.predict \
+            --project_name=idsc_26_predict \
             --predict_parameters_path=predict_config/aneurysm.json \
             --model_path=checkpoints/model.ckpt \
-            --no_edge_feature \
             --prediction_save_path=predictions \
